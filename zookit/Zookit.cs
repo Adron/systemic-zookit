c2ee704baaf709deeec6f8c9fc15d8db00288369
Adron Hall (orange) <adron_hall@homedepot.com>
2016-01-28 17:35:20 -0800

    Renamed Class1.

--- zookit/Zookit.cs
+++ zookit/Zookit.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 
 namespace zookit
 {
-    public class Class1
+    public class Zookit
     {
-        public Class1()
+        public Zookit()
         {
         }
     }
